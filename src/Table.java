e3944f45fbd6076184e131966294d86e463cd98f
karaolinabrylowska <kar.bronk@gmail.com>
2022-09-27 13:01:34 +0200

    Create a method to ascending sorting.

--- src/Table.java
+++ src/Table.java
@@ -1,9 +1,26 @@
 import java.util.Arrays;
 public class Table {
     public static void main(String[] args) {
-        int[] table = {5,3,1,2,4};
+        int[] table = {5, 3, 1, 2, 4};
 
-        System.out.println("Table before sorting "+Arrays.toString(table));
+        System.out.println("Table before sorting " + Arrays.toString(table));
 
+        sorting1(table);
+
+        System.out.println("Table after sorting (ascending)" + Arrays.toString(table));
+    }
+
+    static void sorting1(int[] table) {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = i + 1; j < table.length; j++) {
+                int value1 = table[i];
+                int value2 = table[j];
+
+                if (value1 > value2) {
+                    table[i] = value2;
+                    table[j] = value1;
+                }
+            }
+        }
     }
 }
