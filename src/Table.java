961f8f0b9fa1f38fc8aea88f402d10763b29c244
karaolinabrylowska <kar.bronk@gmail.com>
2022-09-27 12:19:31 +0200

    Created a new array. Array is displayed on screen.

---
+++ src/Table.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Table {
+    public static void main(String[] args) {
+        int[] table = {5,3,1,2,4};
+
+        System.out.println("Table before sorting "+Arrays.toString(table));
+
+    }
+}
